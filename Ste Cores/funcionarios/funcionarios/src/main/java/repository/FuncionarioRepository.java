46408ec2bd3757d9b62b154dc4d1873acdd2fc42
Estêvão Rischter <rischtere@gmail.com>
2025-11-14 14:57:19 -0300

    Delete Ste Cores directory

--- Ste Cores/funcionarios/funcionarios/src/main/java/repository/FuncionarioRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.setecores.funcionarios.model.Funcionario;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-}
